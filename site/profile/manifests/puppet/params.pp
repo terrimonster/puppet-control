2dfb8e00c3d29f4a9b161e338fe05af3c91b6dd4
Greg Kitson <greg.kitson@gmail.com>
2014-11-18 12:54:58 -0500

    these will be deployed by r10k anyway
    Using /vagrant as the r10k remote obviates the need for managing this
    configuration differently for xmaster.

--- site/profile/manifests/puppet/params.pp
+++ site/profile/manifests/puppet/params.pp
@@ -3,16 +3,14 @@
 # classes. This makes your code less messy
 # and follows puppet best practices.
 class profile::puppet::params {
+  $hieradir = '"/etc/puppetlabs/puppet/environments/%{::environment}/hieradata"'
+  $basemodulepath = "${::settings::confdir}/modules:/opt/puppet/share/puppet/modules"
   case $::settings::server {
     'xmaster.vagrant.vm': {
       $remote = '/vagrant'
-      $hieradir = '/vagrant/hieradata'
-      $basemodulepath = "/vagrant/site:${::settings::confdir}/modules:/opt/puppet/share/puppet/modules"
     }
     default: {
       $remote = 'git@github.com:terrimonster/puppet-control.git'
-      $hieradir = '"/etc/puppetlabs/puppet/environments/%{::environment}/hieradata"'
-      $basemodulepath = "${::settings::confdir}/modules:/opt/puppet/share/puppet/modules"
     }
   }
 }
