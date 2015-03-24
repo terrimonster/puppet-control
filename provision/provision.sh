a258b27885405a0805fe492f49653c753984f0ae
Mike Marseglia <mike.marseglia@hasbro.com>
2015-03-24 13:17:05 -0400

    the virutal machine xagent1 is referenced as xagent1 everywhere except in /etc/hosts. changed xagent to xagent1 in /etc/hosts file

--- provision/provision.sh
+++ provision/provision.sh
@@ -16,7 +16,7 @@ cat > /etc/hosts <<EOH
 127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain
 ::1 localhost localhost.localdomain localhost6 localhost6.localdomain
 192.168.137.10 xmaster.vagrant.vm xmaster puppet
-192.168.137.14 xagent.vagrant.vm xagent
+192.168.137.14 xagent1.vagrant.vm xagent1
 EOH
 
 ## Download and extract the PE installer
